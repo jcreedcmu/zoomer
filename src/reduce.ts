156d6d5d806a30a30ec92a2f010d577a41344447
Jason Reed <jcreed@gmail.com>
2025-05-03 14:23:57 -0400

    Pan

--- src/reduce.ts
+++ src/reduce.ts
@@ -1,6 +1,36 @@
 import { produce } from 'immer';
-import { AppState } from './state';
+import { AppState, GameState, MouseState } from './state';
 import { Action } from './action';
+import { Point } from './lib/types';
+import { compose, translate } from './lib/se2';
+import { vsub } from './lib/vutil';
+
+function reduceMouseUp(state: GameState, ms: MouseState, p_in_unit_canvas: Point): GameState {
+  return produce(state, s => { s.mouseState = { t: 'up', p_in_unit_canvas } });
+}
+
+function reduceMouseDown(state: GameState, ms: MouseState, p_in_unit_canvas: Point): GameState {
+  return produce(state, s => {
+    s.mouseState = {
+      t: 'pan',
+      p_in_unit_canvas,
+    }
+  });
+
+}
+
+function reduceMouseMove(state: GameState, ms: MouseState, p_in_unit_canvas: Point): GameState {
+  switch (ms.t) {
+    case 'up': return produce(state, s => { s.mouseState = { t: 'up', p_in_unit_canvas } });
+    case 'pan': {
+      const new_game_from_unit_canvas = compose(state.game_from_unit_canvas, translate(vsub(ms.p_in_unit_canvas, p_in_unit_canvas)));
+      return produce(state, s => {
+        s.mouseState = { t: 'pan', p_in_unit_canvas };
+        s.game_from_unit_canvas = new_game_from_unit_canvas;
+      });
+    }
+  }
+}
 
 export function reduce(state: AppState, action: Action): AppState {
   switch (action.t) {
@@ -10,11 +40,20 @@ export function reduce(state: AppState, action: Action): AppState {
     case 'side-effect': {
       return state;
     }
-    case 'mouseDown': {
-      const { x, y } = action.p_in_unit_canvas;
-      return state;
+    case 'mouseMove': {
+      const ms = state.gameState.mouseState;
+      const newGameState = reduceMouseMove(state.gameState, ms, action.p_in_unit_canvas);
+      return produce(state, s => { s.gameState = newGameState; });
     }
-    case 'mouseMove': { console.log('mousemove', action.p_in_unit_canvas); return state; }
-    case 'mouseUp': { console.log('mouseup', action.p_in_unit_canvas); return state; }
+    case 'mouseUp': {
+      const ms = state.gameState.mouseState;
+      const newGameState = reduceMouseUp(state.gameState, ms, action.p_in_unit_canvas);
+      return produce(state, s => { s.gameState = newGameState; });
+    }
+    case 'mouseDown': {
+      const ms = state.gameState.mouseState;
+      const newGameState = reduceMouseDown(state.gameState, ms, action.p_in_unit_canvas);
+      return produce(state, s => { s.gameState = newGameState; });
+    }
   }
 }
--- src/state.ts
+++ src/state.ts
@@ -10,11 +10,12 @@ export type AppState = {
 }
 
 export type MouseState =
-  | { t: 'pan', p_in_canvas: Point }
-  | { t: 'up', p_in_canvas: Point }
+  | { t: 'pan', p_in_unit_canvas: Point }
+  | { t: 'up', p_in_unit_canvas: Point }
   ;
 
 export type GameState = {
+  mouseState: MouseState,
   game_from_unit_canvas: SE2, // maps the unit square [0,1] x [0,1] to game world coordinates
 }
 
@@ -22,6 +23,9 @@ export function mkState(): AppState {
   return {
     counter: 0, effects: [], debugStr: '',
     gameState:
-      { game_from_unit_canvas: mkSE2({ x: 1, y: 1 }, { x: 0, y: 0 }) }
+    {
+      mouseState: { t: 'up', p_in_unit_canvas: { x: 0, y: 0 } },
+      game_from_unit_canvas: mkSE2({ x: 1, y: 1 }, { x: 0, y: 0 }),
+    }
   };
 }
